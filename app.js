7fc25aa581443e58cb7d589a6f98528635229384
Stephen Florian <steezeven@gmail.com>
2015-08-14 11:15:44 -0600

    mvp goal 2 complete

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 $(document).ready(function(){
+
+  //Tracks energy level and appends to the #status div. 
   var userEnergy = 100;
-  var energyP = document.createElement('p');
+  var energyP = document.createElement('div');
   var energyText = document.createTextNode('energy: ' + userEnergy);
   var energyTracker = energyP.appendChild(energyText);
   $(energyTracker).appendTo('#status');
-  console.log(energyP,energyText,energyTracker);
+
+  //initialize step tracker, appends to #status and sets display: none
+  var stepCounter = 0;
+  var stepDiv = document.createElement('div');
+  stepDiv.setAttribute('id', 'steps');
+  $(stepDiv).appendTo('#status');
+
+  //Text dialgougegeg
+  
+
+  //Sets behavior for the walk button. Increments stepCounter by 1 and sets #steps to display: true.
+  $('#walk').click(function(){
+    this.setAttribute('disabled', true);
+    setTimeout(function(){
+      $('#walk').removeAttr('disabled')
+    }, 3000);
+    $('#steps').css('display', 'block');
+    stepCounter += 1;
+    stepDiv.innerHTML = 'steps: ' + stepCounter;
+    var walking = document.createElement('div');
+    walking.innerHTML = 'you continue down the trail';
+    $('#left').prepend(walking);
+  });
 });
 
 
